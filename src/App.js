e22686d5ac6f6880f8e089d98e537d3688aefac4
Aleksandr <adorofeev@team.amocrm.com>
2019-01-15 01:24:45 +0300

    starting

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
+import Main from './components/Main/Main';
+import Sidebar from './components/Sidebar/Sidebar';
+
+const App = () => {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React!
-          </a>
-        </header>
+        <Sidebar />
+        <Main />
       </div>
     );
-  }
 }
 
 export default App;
