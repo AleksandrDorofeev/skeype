1849c26853ec71fae196efc133d74882cc423fcc
Aleksandr <adorofeev@team.amocrm.com>
2019-02-05 01:11:17 +0300

    start chat

--- src/containers/User/User.js
+++ src/containers/User/User.js
@@ -1,15 +1,23 @@
 import React from "react";
 import "./User.scss";
+import store from '../../store/store';
+import { setActiveUserId } from '../../action/action'
+
 const User = ({ user }) => {
- const { name, profile_pic, status } = user;
- return (
- <div className="User">
- <img src={profile_pic} alt={name} className="User__pic" />
- <div className="User__details">
- <p className="User__details-name">{name}</p>
- <p className="User__details-status">{status}</p>
- </div>
- </div>
- );
+  const { name, profile_pic, status } = user;
+  return (
+    <div className="User" onClick={handleUserClick.bind(null, user)}>
+      <img src={profile_pic} alt={name} className="User__pic" />
+      <div className="User__details">
+        <p className="User__details-name">{name}</p>
+        <p className="User__details-status">{status}</p>
+      </div>
+    </div>
+  );
 };
+
+function handleUserClick({ user_id }) {
+  store.dispatch(setActiveUserId(user_id))
+}
+
 export default User;
--- src/reducers/activeUserIdReducer.js
+++ src/reducers/activeUserIdReducer.js
@@ -1,3 +1,11 @@
+import { SET_ACTIVE_USER_ID } from '../constants/action-types'
+
 export default function activeUserIdReducer(state = null, action) {
-  return state;
+  switch (action.type) {
+    case SET_ACTIVE_USER_ID:
+      return action.payload;
+
+      default:
+        return state;
+  }
 }
