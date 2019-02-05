0f0bbae42f2e847a80255d0ee9c1a87259b6fd36
Aleksandr <adorofeev@team.amocrm.com>
2019-02-06 00:17:10 +0300

    final

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,14 @@
 import userReducer from './userReducer';
 import contactsReducer from './contactsReducer';
 import activeUserIdReducer from './activeUserIdReducer';
+import messagesReducer from './messagesReducer';
+import typingReducer from './typingReducer'
 import { combineReducers } from 'redux';
 
 export default combineReducers({
   user: userReducer,
   contacts: contactsReducer,
-  activeUserId: activeUserIdReducer
+  activeUserId: activeUserIdReducer,
+  messages: messagesReducer,
+  typing: typingReducer
 })
